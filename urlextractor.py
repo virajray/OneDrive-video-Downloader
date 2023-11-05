064ce30409ad79d15756b0e08a9bb5a4f1a4ac26
Viraj Rathnayake <36956604+virajray@users.noreply.github.com>
2023-11-05 20:37:37 +0530

    Update urlextractor.py

--- urlextractor.py
+++ urlextractor.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 # Set the URL of the SharePoint web page
-sharepoint_url = str(input("Enter your sharepoit url: "))
+sharepoint_url = str(input("Enter your sharepoint url: "))
 
 # Initialize the web driver (make sure to specify the path to your WebDriver)
 driver = webdriver.Chrome(executable_path='path/to/chromedriver')
@@ -15,7 +15,7 @@ driver.find_element_by_tag_name('body').send_keys(Keys.F12)  # Open developer to
 driver.find_element_by_id('tab-network').click()  # Click on the Network tab
 
 # Allow time for the network tab to load (adjust the time as needed)
-driver.implicitly_wait(5)
+driver.implicitly_wait(8)
 
 # Find and extract the URL
 network_entries = driver.find_elements_by_css_selector('.netRow')
